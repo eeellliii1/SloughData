0b2fa4a65582bc58ca9973aed46191b23fbe67db
s-araoe <s-araoe@53S50020328.it.bsd405.org>
2017-10-19 11:41:36 -0700

    Created initial class for making basic program

---
+++ src/InitialProj.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author s-araoe
+ *
+ */
+public class InitialProj {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
