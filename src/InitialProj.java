b603d855a0b6dfddb89f6e9f1a6c297954309d83
s-araoe <s-araoe@53S50020328.it.bsd405.org>
2018-02-08 11:35:23 -0800

    Fixed user directory messages

--- src/InitialProj.java
+++ src/InitialProj.java
@@ -7,32 +7,61 @@
  *
  */
 import java.util.*;
+import java.util.regex.Pattern;
+import java.io.*;
 public class InitialProj {
 
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String word = "";
-		
+		String fileName = "";
 		Scanner reader = new Scanner(System.in);
-		while(!(word.equals("Stop")) && !(word.equals("stop")))
+		String dir = System.getProperty("user.dir");
+		System.out.println(dir);
+		String[] userSplit = dir.split(Pattern.quote("\\"));
+		System.out.println(userSplit[2]);
+		String user = userSplit[2];
+		
+		System.setProperty("user.dir", "c:\\Users\\" + user + "\\Desktop");
+
+// if the directory does not exist, create it
+		
+		boolean desktop = new File("c:\\Users\\" + user + "\\Desktop").mkdirs();
+		
+//Loop that runs the program		
+		while(!(word.toLowerCase().equals("stop")))
 		{
-			//Get input to decide next step
+//Gets input to decide next step
 			System.out.println("What would you like to do? Write, Read, or enter Stop to exit");
 			word = reader.nextLine();
-			if(word.equals("write") || word.equals("Write"))
+			if(word.toLowerCase().equals("write"))
 			{
-				writer();
+				System.out.println("Enter the name of the file you wish to write:");
+				fileName = reader.nextLine();
+				try 
+				{
+					writer(fileName);
+				} 
+				catch (IOException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			else if(word.equals("read") || word.equals("Read"))
+			else if(word.toLowerCase().equals("read"))
 			{
-				reader();
+				//System.setProperty("user.dir", "c:\\Users\\" + user + "\\Desktop")
+				System.out.println("Enter the name of the file you wish to write:");
+				fileName = reader.nextLine();
+				reader(fileName);
 			}
 			else
 			{
-				if(!(word.equals("stop")) && !(word.equals("Stop")))
+				if(!(word.toLowerCase().equals("stop")))
 				{
 					System.out.println("Invalid input. Please enter a valid input \n\n\n\n");
 				}
@@ -46,15 +75,46 @@ public class InitialProj {
 	}
 	
 	//Writing method
-	public static void writer()
+	public static void writer(String fileName) throws IOException
 	{
+		String contents = reader(fileName);
+		StringBuilder appender = new StringBuilder(contents);
+		String text = "";
+		Scanner fileWrSc = new Scanner(System.in);
 		System.out.println("Writes---------------");
+		BufferedWriter fileWrite = new BufferedWriter(new FileWriter(fileName));
+		text = fileWrSc.nextLine();
+		appender.append(" " + text);
+		fileWrite.write("" + appender);
+		fileWrite.close();
 	}
 	
-	//Reading method
-	public static void reader()
+//Reading method
+	public static String reader(String fileName)
 	{
-		System.out.println("Reads---------------");
+		String text = "";
+		//System.out.println("Reads---------------");
+		String nextLine = null;
+		try
+		{
+			FileReader fileRead = new FileReader(fileName);
+			BufferedReader buffFileRead = new BufferedReader(fileRead);
+//Keep reading the file while there is file left to be read
+			while((nextLine = buffFileRead.readLine()) != null)
+			{
+				System.out.println(nextLine + "\n");
+				text += nextLine;
+			}
+		}
+		catch(FileNotFoundException exc)
+		{
+			System.out.println("File not found!\nCreating File...");
+		}
+		catch(IOException exc)
+		{
+			System.out.println("IO Exception");
+		}
+		return text;
 	}
 
 }
